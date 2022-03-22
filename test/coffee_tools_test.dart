f1f5706e9d5c8cd33d62ba4f5bfc0e20ebcb173b
Jonathan <jonathan956g@gmail.com>
2022-03-22 14:11:02 -0500

    unit tests for funcitons

--- test/coffee_tools_test.dart
+++ test/coffee_tools_test.dart
@@ -18,7 +18,60 @@ void main() {
     });
 
     test('throws ArgumentError on negative number', () {
-      expect(() => CoffeeTools.cupsToOunces(0), throwsArgumentError);
+      expect(() => CoffeeTools.cupsToOunces(-1), throwsArgumentError);
+    });
+  });
+
+  group("ouncesToGrams", () {
+    test('calculates grams in 1 cup', () {
+      var ounces = CoffeeTools.cupsToOunces(1);
+      var waterGrams = CoffeeTools.ouncesToGrams(ounces);
+
+      expect(waterGrams, 177.42);
+    });
+
+    test('throws ArgumentError on zero', () {
+      expect(() => CoffeeTools.ouncesToGrams(0), throwsArgumentError);
+    });
+
+    test('throws ArgumentError on negative number', () {
+      expect(() => CoffeeTools.ouncesToGrams(-1), throwsArgumentError);
+    });
+  });
+
+  group("frenchPress", () {
+    test('calculates grams of coffee', () {
+      var ounces = CoffeeTools.cupsToOunces(1);
+      var waterGrams = CoffeeTools.ouncesToGrams(ounces);
+      var coffeeGrams = CoffeeTools.frenchPress(waterGrams);
+
+      expect(coffeeGrams, 12.67);
+    });
+
+    test('throws ArgumentError on zero', () {
+      expect(() => CoffeeTools.frenchPress(0), throwsArgumentError);
+    });
+
+    test('throws ArgumentError on negative number', () {
+      expect(() => CoffeeTools.frenchPress(-1), throwsArgumentError);
+    });
+  });
+
+  group("dripMachine", () {
+    test('calculates grams of coffee', () {
+      var ounces = CoffeeTools.cupsToOunces(1);
+      var waterGrams = CoffeeTools.ouncesToGrams(ounces);
+      var coffeeGrams = CoffeeTools.dripMachine(waterGrams);
+
+      expect(coffeeGrams, 10.44);
+    });
+
+    test('throws ArgumentError on zero', () {
+      expect(() => CoffeeTools.dripMachine(0), throwsArgumentError);
+    });
+
+    test('throws ArgumentError on negative number', () {
+      expect(() => CoffeeTools.dripMachine(-1), throwsArgumentError);
     });
   });
 }
